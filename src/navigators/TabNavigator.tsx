7db33b73707b799386b0a95395da0313dc514935
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-11-23 01:50:09 +0530

    tab navigation styles commit

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -6,6 +6,7 @@ import CartScreen from '../screens/CartScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import OrderHistoryScreen from '../screens/OrderHistoryScreen';
 import CustomIcon from '../components/CustomIcon';
+import { COLORS } from '../theme/theme';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,7 +16,7 @@ const TabNavigator = () => {
       tabBarHideOnKeyboard: true,
       headerShown: false,
       tabBarShowLabel: false,
-      
+      tabBarStyle: styles.tabBarStyle,
     }}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Cart" component={CartScreen} />
@@ -25,6 +26,16 @@ const TabNavigator = () => {
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    height: 80,
+    position: 'absolute',
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopWidth: 0,
+    elevation: 0,
+    borderTopColor: 'transparent',
+
+  },
+});
 
 export default TabNavigator
