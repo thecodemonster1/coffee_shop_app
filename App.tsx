708746ca41dbe2a15fbb9ac5d6ad60cb38aebd0f
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-11-23 18:23:13 +0530

    tab nav commits

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './src/navigators/TabNavigator';
@@ -14,24 +14,26 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-     <NavigationContainer>
-      {/* When you don't want the header bar make headerShown to false 👇*/}
-      <Stack.Navigator screenOptions={{headerShown: false}}> 
-        <Stack.Screen 
-          name="Tab" 
-          component={TabNavigator} 
-          options={{animation: 'slide_from_bottom'}}/>
-        <Stack.Screen 
-          name="Details" 
-          component={DetailsScreen} 
-          options={{animation: 'slide_from_bottom'}}/>
-        <Stack.Screen 
-          name="Payments" 
-          component={PaymentScreen} 
-          options={{animation: 'slide_from_bottom'}}/>
-        
-      </Stack.Navigator>
-     </NavigationContainer>
+    // <SafeAreaView>
+      <NavigationContainer>
+        {/* When you don't want the header bar make headerShown to false 👇*/}
+        <Stack.Navigator screenOptions={{headerShown: false}}> 
+          <Stack.Screen 
+            name="Tab"
+            component={TabNavigator} 
+            options={{animation: 'slide_from_bottom'}}/>
+          <Stack.Screen 
+            name="Details" 
+            component={DetailsScreen} 
+            options={{animation: 'slide_from_bottom'}}/>
+          <Stack.Screen 
+            name="Payments" 
+            component={PaymentScreen} 
+            options={{animation: 'slide_from_bottom'}}/>
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+    // </SafeAreaView>
   )
 }
 
--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -7,8 +7,11 @@ import FavoritesScreen from '../screens/FavoritesScreen';
 import OrderHistoryScreen from '../screens/OrderHistoryScreen';
 import CustomIcon from '../components/CustomIcon';
 import { COLORS } from '../theme/theme';
+import { BlurView } from '@react-native-community/blur';
 
 const Tab = createBottomTabNavigator();
+const tabBar_active = COLORS.primaryOrangeHex;
+const tabBar_unactive = COLORS.primaryGreyHex;
 
 const TabNavigator = () => {
   return (
@@ -17,11 +20,54 @@ const TabNavigator = () => {
       headerShown: false,
       tabBarShowLabel: false,
       tabBarStyle: styles.tabBarStyle,
+      tabBarBackground:() => (
+        <BlurView overlayColor='' blurAmount={50} style={styles.BlurViewStyles} />
+      )
     }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Cart" component={CartScreen} />
-      <Tab.Screen name="Favorite" component={FavoritesScreen} />
-      <Tab.Screen name="History" component={OrderHistoryScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: ({ focused, color, size }) => (
+          <CustomIcon 
+            name="home" 
+            size={25} 
+            color={
+              focused?tabBar_active : tabBar_unactive
+            } 
+          />
+        )
+      }}/>
+      <Tab.Screen name="Cart" component={CartScreen} options={{
+        tabBarIcon: ({ focused, color, size }) => (
+          <CustomIcon 
+            name="cart" 
+            size={25} 
+            color={
+              focused?tabBar_active : tabBar_unactive
+            } 
+          />
+        )
+      }}/>
+      <Tab.Screen name="Favorite" component={FavoritesScreen} options={{
+        tabBarIcon: ({ focused, color, size }) => (
+          <CustomIcon 
+            name="like" 
+            size={25} 
+            color={
+              focused?tabBar_active : tabBar_unactive
+            } 
+          />
+        )
+      }}/>
+      <Tab.Screen name="History" component={OrderHistoryScreen} options={{
+        tabBarIcon: ({ focused, color, size }) => (
+          <CustomIcon 
+            name="bell" 
+            size={25} 
+            color={
+              focused?tabBar_active : tabBar_unactive
+            } 
+          />
+        )
+      }}/>
     </Tab.Navigator>
   );
 }
@@ -34,7 +80,10 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     elevation: 0,
     borderTopColor: 'transparent',
-
+  },
+  BlurViewStyles: {
+    position: 'absolute',
+    
   },
 });
 
