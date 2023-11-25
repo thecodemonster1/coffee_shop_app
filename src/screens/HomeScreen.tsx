4b8234836457df165e13a043d78b864d622fc76e
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-11-25 19:16:56 +0530

    Added new function (getCoffeeList)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -16,6 +16,15 @@ const getCategoriesFromData = (data: any) => {
   return categories;
 };
 
+const getCoffeeList = (category: string, data: any) => {
+  if (category == 'All'){
+    return data;
+  }else{
+    let coffeelist = data.filter((item: any) => item.name == category);
+    return coffeelist;
+  }
+}
+
 const HomeScreen = () => {
   const CoffeeList = useStore((state: any) => state.CoffeeList);
   const BeanList = useStore((state: any) => state.BeanList);
