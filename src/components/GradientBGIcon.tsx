22bdd0fe63b7112f6f817687da6027926a6f30b0
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-11-30 00:37:11 +0530

    Header Bar config in process

--- src/components/GradientBGIcon.tsx
+++ src/components/GradientBGIcon.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient';
-import {COLORS, SPACING} from '../theme/theme';
-import CustomIcon from './CustomIcon';
+import { COLORS } from '../theme/theme';
 
 interface GradientBGIconProps {
   name: string;
@@ -10,36 +9,20 @@ interface GradientBGIconProps {
   size: number;
 }
 
-const GradientBGIcon: React.FC<GradientBGIconProps> = ({name, color, size}) => {
+const GradientBGIcon = () => {
   return (
     <View style={styles.Container}>
-      <LinearGradient
-        start={{x: 0, y: 0}}
-        end={{x: 1, y: 1}}
-        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
-        style={styles.LinearGradientBG}>
-        <CustomIcon name={name} color={color} size={size} />
-      </LinearGradient>
+      {/* 1:30 in Video */}
+      <linearGradient 
+        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]} 
+        style={styles.LinearGradientBG}></linearGradient>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  Container: {
-    borderWidth: 2,
-    borderColor: COLORS.secondaryDarkGreyHex,
-    borderRadius: SPACING.space_12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: COLORS.secondaryDarkGreyHex,
-    overflow: 'hidden',
-  },
-  LinearGradientBG: {
-    height: SPACING.space_36,
-    width: SPACING.space_36,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  Container: {},
+  LinearGradientBG: {},
+})
 
-export default GradientBGIcon;
+export default GradientBGIcon
