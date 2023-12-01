6728689d33922e3af3b6e9a3aca87539ecfcf8dd
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-12-01 22:35:48 +0530

    Added a search coffee feture

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useStore } from '../store/store';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { COLORS } from '../theme/theme';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
 import HeaderBar from '../components/HeaderBar';
+import CustomIcon from '../components/CustomIcon';
 
 const getCategoriesFromData = (data: any) => {
   let temp:any = {};
@@ -34,7 +35,7 @@ const HomeScreen = () => {
   const [categories, setCategories] = useState(
     getCategoriesFromData(CoffeeList),
   ); // (undefined)
-  const [serchText, setSearchText] = useState(); // (undefined)
+  const [serchText, setSearchText] = useState(''); // (undefined)
   const [categoryIndex, setCategoryIndex] = useState({
     index: 0,
     category: categories[0],
@@ -52,6 +53,35 @@ const HomeScreen = () => {
         contentContainerStyle={styles.ScrollViewFlex}>
           {/* App Header */}
           <HeaderBar title={"Home Screen"}/>
+
+          <Text style={styles.ScreenTitle}>
+            Find the Best{'\n'}Coffee For You...
+          </Text>
+
+          {/* Search Input */}
+          <View style={styles.InputContainerComponent}>
+            <TouchableOpacity onPress={() => {}} >
+              <CustomIcon 
+                style={styles.InputIcon}
+                name="search" 
+                size={FONTSIZE.size_18} 
+                color={
+                  (serchText.length > 0)
+                  ? COLORS.primaryOrangeHex
+                  : COLORS.primaryLightGreyHex 
+                } 
+              />
+            </TouchableOpacity>
+            <TextInput 
+              placeholder='Find your coffee...' 
+              value={serchText} 
+              onChangeText={text => setSearchText(text)} 
+              placeholderTextColor={COLORS.primaryLightGreyHex}
+              style={styles.TextInputContainer}
+            />
+          </View>
+
+          
       </ScrollView>
     </View>
   )
@@ -65,7 +95,30 @@ const styles = StyleSheet.create({
   },
   ScrollViewFlex: {
     flexGrow: 1,
-  }
+  },
+  ScreenTitle: {
+    fontSize: FONTSIZE.size_28,
+    fontFamily: FONTFAMILY.poppins_semibold,
+    color: COLORS.primaryWhiteHex,
+    paddingLeft: SPACING.space_30,
+  },
+  InputContainerComponent: {
+    flexDirection: 'row', 
+    margin: SPACING.space_30,
+    borderRadius: BORDERRADIUS.radius_20,
+    backgroundColor: COLORS.primaryDarkGreyHex,
+    alignItems: 'center',
+  },
+  InputIcon: {
+    marginHorizontal: SPACING.space_20,
+  },
+  TextInputContainer: {
+    flex: 1,
+    height: SPACING.space_20 * 3,
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.primaryWhiteHex,
+  },
 })
 
 export default HomeScreen;
