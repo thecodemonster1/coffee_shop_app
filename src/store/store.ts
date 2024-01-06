059ac77578993da7dd3eb5d892e15fc47779864e
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-01-06 23:41:03 +0530

    some bug fixes

--- src/store/store.ts
+++ src/store/store.ts
@@ -15,49 +15,58 @@ export const useStore = create(
       CartList: [],
       OrderHistoryList: [],
       addToCart: (cartItem: any) => 
-      set(
-        produce(state => {
-          let found = false;
-          for (let i = 0; i < state.CartList.length; i++){
-            if (state.CartList[i].id == cartItem.id){
-              found = true
-              let size  = false
-              for (let j = 0; j < state.CartList[i].prices.length; j++){
-                if (state.CartList[i].prices[j].size == cartItem.prices[0].size){
-                  size = true
-                  state.CartList[i].prices[j].quanity++
-                  break;
+        set(
+          produce(state => {
+            let found = false;
+            for (let i = 0; i < state.CartList.length; i++){
+              if (state.CartList[i].id == cartItem.id){
+                found = true
+                let size  = false
+                for (let j = 0; j < state.CartList[i].prices.length; j++){
+                  if (state.CartList[i].prices[j].size == cartItem.prices[0].size){
+                    size = true
+                    state.CartList[i].prices[j].quanity++
+                    break;
+                  }
                 }
+                if (size == false){
+                  state.CartList[i].prices.push(cartItem.prices[0]);
+                }
+                state.CartList[i].price.sort((a: any, b: any) => {
+                  if (a.size > b.size){
+                    return -1;
+                  }
+                  if (a.size < b.size){
+                    return 1;
+                  }
+                  return 0;
+                });
+                break;
               }
-              if (size == false){
-                state.CartList[i].prices.push(cartItem.prices[0]);
-              }
-              state.CartList[i].price.sort((a: any, b: any) => {
-                if (a.size > b.size){
-                  return -1;
-                }
-                if (a.size < b.size){
-                  return 1;
-                }
-                return 0;
-              });
-              break;
             }
-          }
-          if (found == false){
-            state.CartList.push(cartItem);
-          }
-        }),
-      ),
-      calculateCartPrice: () => set(produce(state => {
-        let totalPrice = 0;
-        for (let i = 0; i < state.CartList.length; i++) {
-          let tempprice = 0;
-          for (let j = 0; j < state.CartList[i].prices.length; j++) {
-            tempprice += (parseFloat(state.CartList[i].prices[j].price) * state.CartList[i].prices[j].quanity);
-          }
-        }
-      })),
+            if (found == false){
+              state.CartList.push(cartItem);
+            }
+          })
+        ),
+      calculateCartPrice: () =>
+        set(
+          produce(state => {
+            let totalprice = 0;
+            for (let i = 0; i < state.CartList.length; i++) {
+              let tempprice = 0;
+              for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                tempprice =
+                  tempprice +
+                  parseFloat(state.CartList[i].prices[j].price) *
+                    state.CartList[i].prices[j].quantity;
+              }
+              state.CartList[i].ItemPrice = tempprice.toFixed(2).toString();
+              totalprice = totalprice + tempprice;
+            }
+            state.CartPrice = totalprice.toFixed(2).toString();
+          }),
+        ),
     }),
     {
       name: 'coffee-app', 
