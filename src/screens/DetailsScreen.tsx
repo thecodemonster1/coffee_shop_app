d03aa453c303ea5d049612b1993bcd5159cb9667
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-01-19 00:43:03 +0530

    update the DetailsScreen datas

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {useStore} from '../store/store';
 
 const DetailsScreen = ({navigation, route}: any) => {
-  console.log('route = ', route.params); // confirm (test) the parameters are working
+  // console.log('route = ', route.params); // confirm (test) the parameters are working
+  const itemsOfIndex = useStore((state: any) =>
+    route.params.type == 'coffee' ? state.CoffeeList : state.BeanList,
+  )[route.params.index];
   return (
     <View>
       <Text>DetailsScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default DetailsScreen
+export default DetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
