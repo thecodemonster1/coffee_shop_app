217faacb107582add2a4c6f4d75246fa0309d82d
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-01-29 21:15:48 +0530

    price footer styles are completed

--- src/components/PaymentFooter.tsx
+++ src/components/PaymentFooter.tsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
 
 interface PriceProps {
   price: string;
@@ -33,13 +34,43 @@ const PaymentFooter: React.FC<PaymentFooterProps> = ({
 };
 
 const styles = StyleSheet.create({
-  PriceFooter: {},
-  PriceContainer: {},
-  PriceTitle: {},
-  PriceText: {},
-  Price: {},
-  PayButton: {},
-  ButtonText: {},
+  PriceFooter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: SPACING.space_20,
+    padding: SPACING.space_20,
+  },
+  PriceContainer: {
+    alignItems: 'center',
+    width: 100,
+  },
+  PriceTitle: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.secondaryLightGreyHex,
+  },
+  PriceText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.primaryOrangeHex,
+  },
+  Price: {
+    color: COLORS.primaryWhiteHex,
+  },
+  PayButton: {
+    backgroundColor: COLORS.primaryOrangeHex,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: SPACING.space_36 * 1.5,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  ButtonText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
 });
 
 export default PaymentFooter;
