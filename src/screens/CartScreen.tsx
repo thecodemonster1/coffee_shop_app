ea4b7f44a503d8e9a257e84f479ed261cc269f81
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-02-03 23:49:10 +0530

    bug fix

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {useStore} from '../store/store';
 
 const CartScreen = () => {
+  const cartList = useStore((state: any) => state.CartList);
   return (
     <View>
       <Text>CartScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
