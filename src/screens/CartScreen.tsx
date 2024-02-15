0870f5957e71cd26b2fba0979a79965d7c7c7209
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-02-15 21:58:02 +0530

    some updates on CartScreen methods

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -4,7 +4,16 @@ import {useStore} from '../store/store';
 
 const CartScreen = () => {
   const cartList = useStore((state: any) => state.CartList); // initializing the cartList
-  console.log("CartList = ", cartList.length);
+  const cartPrice = useStore((state: any) => state.CartPrice); // getting the total price of items in the cart
+  const incrementCartItemQuantity = useStore(
+    (state: any) => state.incrementCartItemQuantity,
+  );
+  const decrementCartItemQuantity = useStore(
+    (state: any) => state.decrementCartItemQuantity,
+  );
+  // 5:09:58 in video
+  console.log('CartList = ', cartList.length);
+  // console.log('CartPrice = ', cartPrice);
   return (
     <View>
       <Text>CartScreen</Text>
@@ -15,4 +24,3 @@ const CartScreen = () => {
 const styles = StyleSheet.create({});
 
 export default CartScreen;
-
