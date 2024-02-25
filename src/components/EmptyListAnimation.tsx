b3414d0598bcf4236cfb9eefb9de7e13e5974cf0
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-02-25 22:52:37 +0530

    some updates

--- src/components/EmptyListAnimation.tsx
+++ src/components/EmptyListAnimation.tsx
@@ -1,14 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+interface EmptyListAnimationProps {
+  title: string;
+}
+
 const EmptyListAnimation = () => {
-  return (
-    <View>
-      <Text>EmptyListAnimation</Text>
-    </View>
-  );
+  return <View style={styles.EmptyCartContainer}></View>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  EmptyCartContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default EmptyListAnimation;
