4fe6939e503b81252908bafdd432c4bd4031b513
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-03-03 21:36:50 +0530

    small changes in lottieview

--- src/components/EmptyListAnimation.tsx
+++ src/components/EmptyListAnimation.tsx
@@ -1,13 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import LottieView from 'lottie-react-native';
 
 interface EmptyListAnimationProps {
   title: string;
 }
 
 const EmptyListAnimation = () => {
-  return <View style={styles.EmptyCartContainer}></View>;
+  return (
+    <View style={styles.EmptyCartContainer}>
+      <LottieView source={require('../lottie/coffeecup.json')} />
+    </View>
+  );
 };
+// 5:20 in video
 
 const styles = StyleSheet.create({
   EmptyCartContainer: {
