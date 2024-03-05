ce0d8fa9ae3226e610336cc6b18c694c422ea2c3
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-03-05 21:48:08 +0530

    update the styles of lottie text

--- src/components/EmptyListAnimation.tsx
+++ src/components/EmptyListAnimation.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import LottieView from 'lottie-react-native';
+import {COLORS, FONTFAMILY, FONTSIZE} from '../theme/theme';
 
 interface EmptyListAnimationProps {
   title: string;
@@ -29,7 +30,12 @@ const styles = StyleSheet.create({
   LottieStyle: {
     height: 300,
   },
-  LottieText: {},
+  LottieText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.primaryOrangeHex,
+    textAlign: 'center',
+  },
 });
 
 export default EmptyListAnimation;
