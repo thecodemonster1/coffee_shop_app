79c746cf3be5be566805d314d2cb15911f39da2f
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-05-01 22:39:13 +0530

    single view cart item container updated

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -87,9 +87,11 @@ const CartItem: React.FC<CartItemProps> = ({
                 </Text>
               </View>
               <View style={styles.CartItemSizeValueContainer}>
-                <TouchableOpacity style={styles.CartItemIcon} onPress={() => {
-                  decrementCartItemQuantityHandler(id, data.size);
-                }}>
+                <TouchableOpacity
+                  style={styles.CartItemIcon}
+                  onPress={() => {
+                    decrementCartItemQuantityHandler(id, data.size);
+                  }}>
                   <CustomIcon
                     name="minus"
                     size={FONTSIZE.size_10}
@@ -100,9 +102,11 @@ const CartItem: React.FC<CartItemProps> = ({
                     {data.quantity}
                   </Text>
                 </View>
-                <TouchableOpacity style={styles.CartItemIcon} onPress={() => {
-                  incrementCartItemQuantityHandler(id, data.size);
-                }}>
+                <TouchableOpacity
+                  style={styles.CartItemIcon}
+                  onPress={() => {
+                    incrementCartItemQuantityHandler(id, data.size);
+                  }}>
                   <CustomIcon
                     name="add"
                     size={FONTSIZE.size_10}
@@ -118,17 +122,38 @@ const CartItem: React.FC<CartItemProps> = ({
           end={{x: 1, y: 1}}
           colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
           style={styles.CartItemSingleLinearGradient}>
+          <View>
+            <Image
+              source={imagelink_square}
+              style={styles.CartItemSingleImage}
+            />
+          </View>
+          <View style={styles.CartItemSingleInfoContainer}>
             <View>
-              <Image source={imagelink_square} style={styles.CartItemSingleImage}/>
+              <Text style={styles.CartItemTitle}>{name}</Text>
+              <Text style={styles.CartItemSubtitle}>{special_ingredient}</Text>
             </View>
-            <View style={styles.CartItemSingleInfoContainer}>
-              <View>
-                <Text style={styles.CartItemTitle}>{name}</Text>
-                <Text style={styles.CartItemSubtitle}>{special_ingredient}</Text>
+            <View style={styles.CartItemSingleSizeValueContainer}>
+              <View style={styles.SizeBox}>
+                <Text
+                  style={[
+                    styles.SizeText,
+                    {
+                      fontSize:
+                        type == 'Bean' ? FONTSIZE.size_12 : FONTSIZE.size_16,
+                    },
+                  ]}>
+                  {prices[0].size}
+                </Text>
               </View>
+              <Text style={styles.SizeCurrency}>
+                {prices[0].currency}
+                <Text style={styles.SizePrice}>{prices[0].price}</Text>
+              </Text>
             </View>
-            {/* 6:03:35 in video */}
-          </LinearGradient>
+          </View>
+          {/* 6:03:35 in video */}
+        </LinearGradient>
       )}
     </View>
   );
@@ -247,7 +272,16 @@ const styles = StyleSheet.create({
     width: 150,
     borderRadius: BORDERRADIUS.radius_20,
   },
-  CartItemSingleInfoContainer: {},
+  CartItemSingleInfoContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'space-around',
+  },
+  CartItemSingleSizeValueContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
 });
 
 export default CartItem;
