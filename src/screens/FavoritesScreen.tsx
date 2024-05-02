1f0c0937220c2ef9c3e97eabe909000e98b5d448
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-05-02 07:44:03 +0530

    code copied from cartscreen and modified for fav. screen

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,14 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {COLORS, SPACING} from '../theme/theme';
+import CartItem from '../components/CartItem';
+import EmptyListAnimation from '../components/EmptyListAnimation';
+import HeaderBar from '../components/HeaderBar';
+import PaymentFooter from '../components/PaymentFooter';
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({navigation}: any) => {
+  const FavoritesList = useStore((state: any) => state.FavoritesList);
+  const tabBarHeight = useBottomTabBarHeight();
+  const addToFavoriteList = useStore((state: any) => state.addToFavoriteList);
+  const deleteFromFavoriteList = useStore(
+    (state: any) => state.deleteFromFavoriteList,
+  );
+  const ToggleFavorite = (favorites: boolean, type: string, id: string) => {
+    favorites ? deleteFromFavoriteList(type, id) : addToFavoriteList(type, id);
+  };
   return (
-    <View>
-      <Text>FavoritesScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View
+          style={[styles.ScrollViewInnerView, {marginBottom: tabBarHeight}]}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Favorites" />
+            {FavoritesList.length == 0 ? (
+              <EmptyListAnimation title={'No Favorite'} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {FavoritesList .map((data: any) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.push('Details', {
+                        index: data.index,
+                        id: data.id,
+                        type: data.type,
+                      });
+                    }}
+                    key={data.id}></TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </View>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default FavoritesScreen
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+    // backgroundColor: 'red',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default FavoritesScreen;
