522c41bc466d522c7f7ee836c1e355a2a30fac7c
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-05-09 12:03:26 +0530

    some updates

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import {ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {COLORS, FONTSIZE} from '../theme/theme';
+import GradientBGIcon from '../components/GradientBGIcon';
 
 const paymentList = [
   {
@@ -22,17 +24,33 @@ const paymentList = [
     icon: require('../assets/app_images/amazonpay.png'),
     isIcon: false,
   },
-]
+];
 
 const PaymentScreen = () => {
   const [paymentMode, setPaymentMode] = useState('Credit Card');
   return (
-    <View>
-      <Text>PaymentScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}>
+        <View style={styles.HeaderContainer}>
+          <TouchableOpacity >
+            <GradientBGIcon name='left' color={COLORS.primaryBlackHex} size={FONTSIZE.size_16} />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default PaymentScreen
+export default PaymentScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {},
+  HeaderContainer: {},
+});
